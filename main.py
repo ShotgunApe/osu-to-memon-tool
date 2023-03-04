3ef56933267e5b3f656bb6ea3cdc38ba64751d28
ShotgunApe <shotgunayyype@gmail.com>
2023-03-03 20:56:24 -0500

    bpm-conv possibly working

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
 import sys
 
-import ast
-
 grablines = False
 timingpointRaw = []
 
@@ -12,31 +10,72 @@ with open(sys.argv[1], "r") as i:
     #loop
     for line in i.readlines():
         if grablines:
-            #this is dumb
+            #create list for each timing point
+            tempTimingLine = []
+
+            #split accordingly
             temp = line.strip("\n")
-            res = temp.split(',')
-            timingpointRaw.append(res)
+            dataLine = temp.split(',')
+
+            #ignore unnecessary data + append to lineList
+            count = 0
+            if len(dataLine) != 1:
+                if float(dataLine[1]) < 0:
+                    continue
+                for i in dataLine:
+                    if count < 2:
+                        tempTimingLine.append(float(i))
+                    count = count + 1
+
+                #append to entire timing data
+                timingpointRaw.append(tempTimingLine)
 
         if line == "[TimingPoints]\n" or (line == "\n" and grablines):
-            #something to grab data ig
-            print("good")
             grablines = not grablines 
 
-print(timingpointRaw)
 #temp
-start_offset = 3.2
 
 #create data
+start_offset = float(timingpointRaw[0][0]/1000)
 #convert .osu bpm data into .memon-friendly format
+bpm_data = []
+beat = 0
+previousOffset = timingpointRaw[0][0]
+previousTiming = timingpointRaw[0][1]
+firstTimingPoint = True
 
-bpm_data = {}
+#required to keep track in tick form kinda
+beat_data = []
+
+for timing_point in timingpointRaw:
+    beat = 0
+    if firstTimingPoint:
+        realBPM = 1 / timing_point[1] * 60000
+        bpm_data.append({"beat": beat * 240, "bpm": realBPM})
+        firstTimingPoint = False
+        previousOffset = timing_point[0]
+        previousTiming = timing_point[1]
+
+    else:
+        #some calcs made from previous bpm info, add prevoffset at bottom
+        realBPM = 1 / timing_point[1] * 60000
+
+        beat_data.append(int(round((timing_point[0] - previousOffset) / previousTiming)))
+        for i in beat_data:
+            beat = beat + (i * 240)
+        bpm_data.append({"beat": beat, "bpm": realBPM})
+
+        previousOffset = timing_point[0]
+        previousTiming = timing_point[1]
+
+
 
 #buncha buncha json data
 json_data = {"version": "1.0.0",
              "metadata": {},
-             "timing":   {"offset": start_offset},
+             "timing":   {"offset": start_offset, "bpms": bpm_data},
              #this the good shit yo
-             "data":     {"EXT": {"timing": {"offset": start_offset, "bpms": [bpm_data] }, "notes": [] } }
+             "data":     {"EXT": {"level": 10, "resolution": 240, "notes": [] } }
              }
 
 #format data
@@ -45,3 +84,5 @@ json_formatted_str = json.dumps(json_data, indent=4)
 #write to output .memon file
 with open('output.memon', 'w') as f:
     f.write(json_formatted_str)
+
+print("done!")
